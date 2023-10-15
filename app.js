222941c3a8e2b0123af11dde89b9ae32a2c187ee
tashfia <tashfia@cs.washington.edu>
2023-10-14 17:27:21 -0700

    nkndkf

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const app = express();
+
+const sqlite = require('sqlite');
+const sqlite3 = require('sqlite3');
+
+const multer = require('multer');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(multer().none());
+
+const PORT_NUM = 8000;
+
+// get
+
+// all restaruarnt info +  search / filter
+app.get('/dub/data', async (req, res) => {
+  try {
+    let filter = req.query.filter;
+    let search = req.query.search;
+
+    let db = await getDBConnection();
+    if(filter) {
+      let query = "SELECT * FROM restaurants WHERE culture=?";
+      let cuisine = await db.all(query, filter);
+      await db.close();
+      res.json(cuisine);
+    } else if (search) {
+      let query = "SELECT * FROM restaurants WHERE name LIKE ? OR location LIKE ? OR halal LIKE ?" +
+                    "OR GF LIKE ? OR veg LIKE ? OR culture LIKE ?";
+      let item = '%' + search + '%';
+      let place = await db.all(query, [item, item, item, item]);
+      await db.close;
+      res.json(place);
+    } else {
+      let query = 'SELECT * FROM restaurants';
+      let places = await db.all(query);
+      await db.close();
+      res.json(places);
+    }
+  } catch (err) {
+    res.status(500);
+    console.log(err);
+    res.type('text').send('try again');
+  }
+});
+
+// friends - can set what their top 5 spots are
+    // can filter users to see their eithnic background to see what you would enjoy based on them
+    // can search users
+
+// leaderboard gwt it to order from gretest tomlowest in query
+app.get('/dub/data/leaderboard',  async (req, res) => {
+  try {
+    let db = await getDBConnection();
+    let query = "SELECT * FROM restaurants ORDER BY upvotes DESC";
+    let ranked = await db.all(query);
+    await db.close();
+    res.json(ranked);
+  } catch (err) {
+    res.status(500);
+    console.log(err);
+    res.type('text').send('try again');
+  }
+});
+
+// upvoting
+
+// review = new database with fid connecting to id of restaruant and id of user and reviews
+
+
+async function getDBConnection() {
+  const db = await sqlite.open({
+    filename: 'dubdata.db',
+    driver: sqlite3.Database
+  });
+  return db;
+}
+
+app.use(express.static('public'));
+const PORT = process.env.PORT || PORT_NUM;
+app.listen(PORT);
